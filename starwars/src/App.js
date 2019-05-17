833336c6739825f0e66936da5d5518fe287405c7
Dil Sombayeva <dilyasom@gmail.com>
2019-05-17 09:20:37 -0700

    implement pagination

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -8,6 +8,9 @@ class App extends Component {
   super();
   this.state = {
    starwarsChars: [],
+   next: '',
+   previous: '',
+   flag: false,
   };
  }
 
@@ -25,11 +28,34 @@ class App extends Component {
    })
    .then(data => {
     console.log(data);
-    this.setState({ starwarsChars: data.results });
+    this.setState({
+     starwarsChars: data.results,
+     next: data.next,
+     previous: data.previous,
+    });
    })
    .catch(err => {
     throw new Error(err);
    });
+ };
+
+ handleNext = () => {
+  if (this.state.next) {
+   //   console.log(this.state.next);
+   this.getCharacters(this.state.next);
+  }
+  //   else {
+  //    this.setState({ flag: true });
+  //   }
+ };
+ handlePrevious = () => {
+  if (this.state.previous) {
+   //   console.log(this.state.next);
+   this.getCharacters(this.state.previous);
+  }
+  //   else {
+  //    this.setState({ flag: true });
+  //   }
  };
 
  render() {
@@ -43,8 +69,22 @@ class App extends Component {
        return <Char charProps={char} />;
       })}
      </div>
-     <button>Previous</button>
-     <button>Next</button>
+     {/* <a href={this.state.Previous}> */}
+     <button
+      onClick={this.handlePrevious}
+      //   className={`${this.state.flag ? 'noth' : null}`}
+     >
+      Previous
+     </button>
+     {/* </a>
+     <a href={this.state.Next}> */}
+     <button
+      onClick={this.handleNext}
+      //   className={`${this.state.flag ? 'noth' : null}`}
+     >
+      Next
+     </button>
+     {/* </a> */}
     </div>
    </div>
   );
